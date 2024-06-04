5e9ca663de5702a771d739ea01192ee51f01e9df
mhassan1 <marc.j.hassan@gmail.com>
2024-06-04 16:43:16 -0400

    Re-organize integration tests

--- src/__tests__/integration/generateDisclaimer.test.ts
+++ src/__tests__/integration/generateDisclaimer.test.ts
@@ -5,76 +5,34 @@ import NpmPlugin from '@yarnpkg/plugin-npm'
 import { pluginRootDir, getDisclaimer } from '../../utils'
 import { execSync } from 'child_process'
 
-const expectedNonRecursive = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(ppath.join(__dirname as PortablePath, 'fixtures/expected/disclaimer.txt' as PortablePath), 'utf8')
-)
-
-const expectedRecursive = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(
-    ppath.join(__dirname as PortablePath, 'fixtures/expected/disclaimerRecursive.txt' as PortablePath),
-    'utf8'
-  )
-)
-
-const expectedNonRecursiveProduction = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(
-    ppath.join(__dirname as PortablePath, 'fixtures/expected/disclaimerProduction.txt' as PortablePath),
-    'utf8'
-  )
-)
-
-const expectedRecursiveProduction = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(
-    ppath.join(__dirname as PortablePath, 'fixtures/expected/disclaimerRecursiveProduction.txt' as PortablePath),
-    'utf8'
-  )
-)
-
 describe.each(['pnp', 'node-modules', 'pnpm'])('licenses generate-disclaimer (%s)', (linker) => {
   const cwd = npath.join(__dirname, 'fixtures', `test-package-${linker}`)
   beforeAll(() => {
     execSync('yarn', { cwd })
   })
 
-  it('should generate disclaimer', () => {
-    const stdout = execSync('yarn licenses generate-disclaimer', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedNonRecursive)
-  })
-
-  it('should generate disclaimer recursively', () => {
-    const stdout = execSync('yarn licenses generate-disclaimer --recursive', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedRecursive)
-  })
-
-  it('should generate disclaimer for production', () => {
-    const stdout = execSync('yarn licenses generate-disclaimer --production', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedNonRecursiveProduction)
-  })
-
-  it('should generate disclaimer recursively for production', () => {
-    const stdout = execSync('yarn licenses generate-disclaimer --recursive --production', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedRecursiveProduction)
+  it.each([
+    ['non-recursively', '', 'disclaimer.txt'],
+    ['recursively', '--recursive', 'disclaimerRecursive.txt'],
+    ['non-recursively for production', '--production', 'disclaimerProduction.txt'],
+    ['recursively for production', '--recursive --production', 'disclaimerRecursiveProduction.txt']
+  ])(`should generate disclaimer %s`, (description, flags, expected) => {
+    const stdout = execSync(`yarn licenses generate-disclaimer ${flags}`, { cwd }).toString()
+    expect(stdout).toBe(
+      normalizeLineEndings(
+        '\n',
+        xfs.readFileSync(ppath.join(__dirname as PortablePath, `fixtures/expected/${expected}` as PortablePath), 'utf8')
+      )
+    )
   })
 })
 
 describe('getDisclaimer', () => {
   it.each([
-    ['non-recursively', false, false, expectedNonRecursive],
-    ['recursively', true, false, expectedRecursive],
-    ['non-recursively for production', false, true, expectedNonRecursiveProduction],
-    ['recursively for production', true, true, expectedRecursiveProduction]
+    ['non-recursively', false, false, 'disclaimer.txt'],
+    ['recursively', true, false, 'disclaimerRecursive.txt'],
+    ['non-recursively for production', false, true, 'disclaimerProduction.txt'],
+    ['recursively for production', true, true, 'disclaimerRecursiveProduction.txt']
   ])('should generate disclaimer %s', async (description, recursive, production, expected) => {
     const cwd = ppath.join(
       pluginRootDir,
@@ -97,6 +55,11 @@ describe('getDisclaimer', () => {
 
     const disclaimer = await getDisclaimer(project, recursive, production)
 
-    expect(disclaimer).toBe(expected)
+    expect(disclaimer).toBe(
+      normalizeLineEndings(
+        '\n',
+        xfs.readFileSync(ppath.join(__dirname as PortablePath, `fixtures/expected/${expected}` as PortablePath), 'utf8')
+      )
+    )
   })
 })
--- src/__tests__/integration/list.test.ts
+++ src/__tests__/integration/list.test.ts
@@ -6,102 +6,28 @@ import { pluginRootDir, getTree } from '../../utils'
 import { execSync } from 'child_process'
 import { Writable } from 'stream'
 
-const expectedNonRecursive = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(ppath.join(__dirname as PortablePath, 'fixtures/expected/list.txt' as PortablePath), 'utf8')
-)
-
-const expectedRecursive = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(ppath.join(__dirname as PortablePath, 'fixtures/expected/listRecursive.txt' as PortablePath), 'utf8')
-)
-
-const expectedNonRecursiveProduction = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(
-    ppath.join(__dirname as PortablePath, 'fixtures/expected/listProduction.txt' as PortablePath),
-    'utf8'
-  )
-)
-
-const expectedRecursiveProduction = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(
-    ppath.join(__dirname as PortablePath, 'fixtures/expected/listRecursiveProduction.txt' as PortablePath),
-    'utf8'
-  )
-)
-
-const expectedJson = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(ppath.join(__dirname as PortablePath, 'fixtures/expected/listJson.txt' as PortablePath), 'utf8')
-)
-
-const expectedExcludeMetadata = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(
-    ppath.join(__dirname as PortablePath, 'fixtures/expected/listExcludeMetadata.txt' as PortablePath),
-    'utf8'
-  )
-)
-
-const expectedExcludeMetadataJson = normalizeLineEndings(
-  '\n',
-  xfs.readFileSync(
-    ppath.join(__dirname as PortablePath, 'fixtures/expected/listExcludeMetadataJson.txt' as PortablePath),
-    'utf8'
-  )
-)
-
 describe.each(['pnp', 'node-modules', 'pnpm'])('licenses list (%s)', (linker) => {
   const cwd = npath.join(__dirname, 'fixtures', `test-package-${linker}`)
   beforeAll(() => {
     execSync('yarn', { cwd })
   })
 
-  it('should list licenses', () => {
-    const stdout = execSync('yarn licenses list', { cwd }).toString()
-    expect(stdout).toBe(expectedNonRecursive)
-  })
-
-  it('should list licenses recursively', () => {
-    const stdout = execSync('yarn licenses list --recursive', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedRecursive)
-  })
-
-  it('should list licenses for production', () => {
-    const stdout = execSync('yarn licenses list --production', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedNonRecursiveProduction)
-  })
-
-  it('should list licenses recursively for production', () => {
-    const stdout = execSync('yarn licenses list --recursive --production', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedRecursiveProduction)
-  })
-
-  it('should list licenses as json', () => {
-    const stdout = execSync('yarn licenses list --json', { cwd }).toString()
-    expect(stdout).toBe(expectedJson)
-  })
-
-  it('should list licenses without metadata', () => {
-    const stdout = execSync('yarn licenses list --exclude-metadata', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedExcludeMetadata)
-  })
-
-  it('should list licenses without metadata as json', () => {
-    const stdout = execSync('yarn licenses list --json --exclude-metadata', {
-      cwd
-    }).toString()
-    expect(stdout).toBe(expectedExcludeMetadataJson)
+  it.each([
+    ['non-recursively', '', 'list.txt'],
+    ['recursively', '--recursive', 'listRecursive.txt'],
+    ['non-recursively for production', '--production', 'listProduction.txt'],
+    ['recursively for production', '--recursive --production', 'listRecursiveProduction.txt'],
+    ['as json', '--json', 'listJson.txt'],
+    ['without metadata', '--exclude-metadata', 'listExcludeMetadata.txt'],
+    ['without metadata as json', '--json --exclude-metadata', 'listExcludeMetadataJson.txt']
+  ])(`should list licenses %s`, (description, flags, expected) => {
+    const stdout = execSync(`yarn licenses list ${flags}`, { cwd }).toString()
+    expect(stdout).toBe(
+      normalizeLineEndings(
+        '\n',
+        xfs.readFileSync(ppath.join(__dirname as PortablePath, `fixtures/expected/${expected}` as PortablePath), 'utf8')
+      )
+    )
   })
 })
 
@@ -119,11 +45,11 @@ describe('licenses list (node-modules with aliases)', () => {
 
 describe('getTree', () => {
   it.each([
-    ['non-recursively', false, false, false, expectedNonRecursive],
-    ['recursively', true, false, false, expectedRecursive],
-    ['non-recursively for production', false, true, false, expectedNonRecursiveProduction],
-    ['recursively for production', true, true, false, expectedRecursiveProduction],
-    ['exclude metadata', false, false, true, expectedExcludeMetadata]
+    ['non-recursively', false, false, false, 'list.txt'],
+    ['recursively', true, false, false, 'listRecursive.txt'],
+    ['non-recursively for production', false, true, false, 'listProduction.txt'],
+    ['recursively for production', true, true, false, 'listRecursiveProduction.txt'],
+    ['exclude metadata', false, false, true, 'listExcludeMetadata.txt']
   ])('should list licenses %s', async (description, recursive, production, excludeMetadata, expected) => {
     const cwd = ppath.join(
       pluginRootDir,
@@ -162,6 +88,11 @@ describe('getTree', () => {
     })
     await new Promise((resolve) => setImmediate(resolve))
 
-    expect(stdout).toBe(expected)
+    expect(stdout).toBe(
+      normalizeLineEndings(
+        '\n',
+        xfs.readFileSync(ppath.join(__dirname as PortablePath, `fixtures/expected/${expected}` as PortablePath), 'utf8')
+      )
+    )
   })
 })
